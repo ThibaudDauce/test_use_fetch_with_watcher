c303de9bec5cf7d7f921eb8c847f3f11a1970979
Thibaud Dauce <git@hadibut.fr>
2025-10-01 12:54:53 +0200

    Minimal reproduction

---
+++ app/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="someParam = 'test'">Set query param</button>
+
+    {{  data  }}
+
+    {{ error }}
+  </div>
+</template>
+<script setup lang="ts">
+
+const someParam = ref(null as null | string)
+
+watch(someParam, () => {
+  console.log('Some param changed')
+})
+
+const { data, error } = useFetch('https://jsonplaceholder.typicode.com/todos/1', {
+  query: {
+    someParam: someParam,
+  }
+})
+</script>
